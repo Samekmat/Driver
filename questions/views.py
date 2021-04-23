0acc30e2573bfef621f96b9956f9ddbebf31955f
Mateusz Samek <samekmat@gmail.com>
2021-04-23 22:41:40 +0200

    Model question with serializer, some entry project config and views/urls changes

---
+++ questions/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class QuestionView(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
