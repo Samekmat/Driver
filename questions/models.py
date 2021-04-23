79f437cb515ac391e9b39bdaffd053215312ff28
Mateusz Samek <samekmat@gmail.com>
2021-04-23 22:43:46 +0200

    Rename body > desc

--- questions/models.py
+++ questions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Question(models.Model):
     title = models.CharField(max_length=100)
-    body = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
