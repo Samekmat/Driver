1d51f257fd281823f0fa14a4ed8436ada79549ae
Mateusz Samek <samekmat@gmail.com>
2021-04-23 22:44:54 +0200

    body > desc

--- questions/serializers.py
+++ questions/serializers.py
@@ -5,4 +5,4 @@ from .models import Question
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ('id', 'title', 'body')
+        fields = ('id', 'title', 'description')
