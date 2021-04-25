7b91513d36419d7a82afd8c10cbe7e00199793cf
Mateusz Samek <samekmat@gmail.com>
2021-04-25 19:04:14 +0200

    Deleted useless title in model question

--- questions/models.py
+++ questions/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Question(models.Model):
-    title = models.CharField(max_length=100)
     content = models.CharField(max_length=200)
     training = models.ForeignKey('Training', on_delete=models.CASCADE)
 
