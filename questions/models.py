5d28487696d6ec90ac66db791703d986a5e91035
Mateusz Samek <samekmat@gmail.com>
2021-04-25 19:02:00 +0200

    question model fixes and training model added

--- questions/models.py
+++ questions/models.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class Question(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
+    training = models.ForeignKey('Training', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Training(models.Model):
+    question = models.ManyToManyField(Question)
