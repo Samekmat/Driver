19861a5e3d5b1c8bf7ee139ff33cbe96ef2f2702
Mateusz Samek <samekmat@gmail.com>
2021-04-27 19:36:28 +0200

    New view to see single question

--- questions/urls.py
+++ questions/urls.py
@@ -4,8 +4,9 @@ from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-router.register('questions', views.QuestionView)
+router.register('questions', views.QuestionListView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('questions/<int:pk>/', views.QuestionView.as_view()),
     ]
--- questions/views.py
+++ questions/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import viewsets
 from .models import Question
 from .serializers import QuestionSerializer
 
 
-class QuestionView(viewsets.ModelViewSet):
+class QuestionListView(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+
+class QuestionView(APIView):
+    def get(self, request, question_id):
+        question = Question.objects.get(pk=question_id)
+
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
