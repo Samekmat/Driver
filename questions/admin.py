ee9e1a3403e098c7b071d302f8c6de791c87c0b8
Mateusz Samek <samekmat@gmail.com>
2021-04-28 16:21:27 +0200

    All models registred in django admin

--- questions/admin.py
+++ questions/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer, Advice, Training, Category
 
 
 admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Advice)
+admin.site.register(Training)
+admin.site.register(Category)
