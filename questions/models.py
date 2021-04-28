f24b61a95bf2fb3fc0a78e782e8bba1701e17da7
Mateusz Samek <samekmat@gmail.com>
2021-04-28 16:42:53 +0200

    Fixed str methods (non type)

--- questions/models.py
+++ questions/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Question(models.Model):
-    name = models.CharField(max_length=120, null=True)
+    name = models.CharField(max_length=120, null=True, default='nazwa zastepcza pyt')
     content = models.CharField(max_length=200)
     training = models.ForeignKey('Training', on_delete=models.CASCADE, related_name='question_training', null=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name) if self.name else ''
 
 
 class Training(models.Model):
-    name = models.CharField(max_length=120, null=True)
+    name = models.CharField(max_length=120, null=True, default='nazwa zastepcza tren')
     question = models.ManyToManyField(Question, related_name='exercise')
 
     def __str__(self):
-        return self.name
+        return str(self.name) if self.name else ''
 
 
 class Answer(models.Model):
@@ -23,15 +23,12 @@ class Answer(models.Model):
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.content
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return str(self.name) if self.name else ''
 
 
 class Advice(models.Model):
@@ -42,4 +39,4 @@ class Advice(models.Model):
     training = models.ForeignKey(Training, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name) if self.name else ''
