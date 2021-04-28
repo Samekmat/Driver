ec32e0ef6374d81e2de9afe84663fa7bc4a4b27e
Mateusz Samek <samekmat@gmail.com>
2021-04-28 16:29:08 +0200

    Add name field in question model and set str methods

--- questions/models.py
+++ questions/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 
 class Question(models.Model):
+    name = models.CharField(max_length=120, null=True)
     content = models.CharField(max_length=200)
     training = models.ForeignKey('Training', on_delete=models.CASCADE, related_name='question_training', null=True)
 
     def __str__(self):
-        return self.content
+        return self.name
 
 
 class Training(models.Model):
     name = models.CharField(max_length=120, null=True)
     question = models.ManyToManyField(Question, related_name='exercise')
+
+    def __str__(self):
+        return self.name
 
 
 class Answer(models.Model):
@@ -19,9 +23,15 @@ class Answer(models.Model):
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.content
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Advice(models.Model):
