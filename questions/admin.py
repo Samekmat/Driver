cde6100991a9649dc98b8f1765728137cb041207
Mateusz Samek <samekmat@gmail.com>
2021-04-29 20:19:22 +0200

    Comment admin.py

--- questions/admin.py
+++ questions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Question, Answer, Advice, Training, Category
 
-
+'''Registration models in django admin panel'''
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Advice)
